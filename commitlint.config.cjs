6daabc1424fca38e33c8da561eaa03640994a557
ruan-cat <1219043956@qq.com>
2025-12-08 09:17:36 +0800

    🔧 config(commitlint,config): 更新最新的 commitlint-config 配置

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,2 +1,8 @@
-const config = require("@ruan-cat/commitlint-config").default;
-module.exports = config;
+// commitlint.config.cjs
+// @ts-check
+module.exports = require("@ruan-cat/commitlint-config").getUserConfig({
+	config: {
+		// 推荐不打印提交范围
+		isPrintScopes: false,
+	},
+});
